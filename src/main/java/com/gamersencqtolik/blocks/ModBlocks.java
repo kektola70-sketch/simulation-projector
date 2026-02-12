b3e25050216424188be74b90c32ef09ac7afbed3
kektola70-sketch <kektola70@gmail.com>
2026-02-12 21:13:58 +0300

    update src/main/java/com/gamersencqtolik/blocks/ModBlocks.java

--- src/main/java/com/gamersencqtolik/blocks/ModBlocks.java
+++ src/main/java/com/gamersencqtolik/blocks/ModBlocks.java
@@ -1,9 +1,6 @@
  package com.gamersencqtolik.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.GlassBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
