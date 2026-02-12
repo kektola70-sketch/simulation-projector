60f2a1d52a74a1b1056d16575f207fb135696e21
kektola70-sketch <kektola70@gmail.com>
2026-02-12 19:11:04 +0300

    update src/main/java/com/gamersencqtolik/blocks/ProjectorBlock.java

--- src/main/java/com/gamersencqtolik/blocks/ProjectorBlock.java
+++ src/main/java/com/gamersencqtolik/blocks/ProjectorBlock.java
@@ -1,19 +1,36 @@
-public boolean canWork(World world, BlockPos pos) {
-    Set<BlockPos> visited = new HashSet<>();
-    return checkConnection(world, pos, visited);
-}
+package com.gamersencqtolik.blocks;
 
-private boolean checkConnection(World world, BlockPos pos, Set<BlockPos> visited) {
-    for (Direction dir : Direction.values()) {
-        BlockPos neighbor = pos.offset(dir);
-        if (visited.contains(neighbor)) continue;
-        visited.add(neighbor);
+import net.minecraft.block.Block;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.util.Direction;
+import java.util.HashSet;
+import java.util.Set;
 
-        Block block = world.getBlockState(neighbor).getBlock();
-        if (block instanceof CodeMachine) return true; // Нашли машину!
-        if (block instanceof SimulationWire) {
-            if (checkConnection(world, neighbor, visited)) return true;
-        }
+public class ProjectorBlock extends Block {
+    public ProjectorBlock() {
+        super(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3.0f));
     }
-    return false;
+
+    public boolean canWork(World world, BlockPos pos) {
+        Set<BlockPos> visited = new HashSet<>();
+        return checkConnection(world, pos, visited);
+    }
+
+    private boolean checkConnection(World world, BlockPos pos, Set<BlockPos> visited) {
+        for (Direction dir : Direction.values()) {
+            BlockPos neighbor = pos.offset(dir);
+            if (visited.contains(neighbor)) continue;
+            visited.add(neighbor);
+
+            Block block = world.getBlockState(neighbor).getBlock();
+            if (block == ModBlocks.CODE_MACHINE.get()) return true;
+            if (block == ModBlocks.SIM_WIRE.get()) {
+                if (checkConnection(world, neighbor, visited)) return true;
+            }
+        }
+        return false;
+    }
 }
