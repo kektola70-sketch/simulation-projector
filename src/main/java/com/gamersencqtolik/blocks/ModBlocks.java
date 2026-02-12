23f150c70d997a2a883418253bd9ba59fccb9a31
kektola70-sketch <kektola70@gmail.com>
2026-02-12 19:10:12 +0300

    update src/main/java/com/gamersencqtolik/blocks/ModBlocks.java

--- src/main/java/com/gamersencqtolik/blocks/ModBlocks.java
+++ src/main/java/com/gamersencqtolik/blocks/ModBlocks.java
@@ -1,5 +1,24 @@
-public static final RegistryObject<Block> SIM_GLASS = BLOCKS.register("sim_glass",
-    () -> new GlassBlock(AbstractBlock.Properties.create(Material.GLASS)
-        .hardnessAndResistance(-1.0F, 3600000.0F) // Не ломается
-        .sound(SoundType.GLASS)
-        .notSolid()));
+package com.gamersencqtolik.blocks;
+
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "simmod");
+
+    public static final RegistryObject<Block> SIM_GLASS = BLOCKS.register("sim_glass",
+        () -> new GlassBlock(AbstractBlock.Properties.create(Material.GLASS)
+            .hardnessAndResistance(-1.0F, 3600000.0F).sound(SoundType.GLASS).notSolid()));
+
+    public static final RegistryObject<Block> SIM_WIRE = BLOCKS.register("sim_wire", 
+        () -> new SimulationWireBlock());
+
+    public static final RegistryObject<Block> CODE_MACHINE = BLOCKS.register("code_machine",
+        () -> new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3.0f)));
+
+    public static final RegistryObject<Block> SIM_PROJECTOR = BLOCKS.register("sim_projector",
+        () -> new ProjectorBlock());
+}
