61ff58e59e397a036bb8d368827c8bddfed3cae7
kektola70-sketch <kektola70@gmail.com>
2026-02-12 19:29:55 +0300

    update src/main/java/com/gamersencqtolik/blocks/ModItems.java

--- src/main/java/com/gamersencqtolik/blocks/ModItems.java
+++ src/main/java/com/gamersencqtolik/blocks/ModItems.java
@@ -1,8 +1,6 @@
 package com.gamersencqtolik.blocks;
 
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,14 +9,14 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "simmod");
 
     public static final RegistryObject<Item> SIM_GLASS = ITEMS.register("sim_glass",
-            () -> new BlockItem(ModBlocks.SIM_GLASS.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
+        () -> new BlockItem(ModBlocks.SIM_GLASS.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
 
     public static final RegistryObject<Item> SIM_WIRE = ITEMS.register("sim_wire",
-            () -> new BlockItem(ModBlocks.SIM_WIRE.get(), new Item.Properties().group(ItemGroup.REDSTONE)));
+        () -> new BlockItem(ModBlocks.SIM_WIRE.get(), new Item.Properties().group(ItemGroup.REDSTONE)));
 
     public static final RegistryObject<Item> CODE_MACHINE = ITEMS.register("code_machine",
-            () -> new BlockItem(ModBlocks.CODE_MACHINE.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
+        () -> new BlockItem(ModBlocks.CODE_MACHINE.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
 
     public static final RegistryObject<Item> SIM_PROJECTOR = ITEMS.register("sim_projector",
-            () -> new BlockItem(ModBlocks.SIM_PROJECTOR.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
+        () -> new BlockItem(ModBlocks.SIM_PROJECTOR.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
 }
