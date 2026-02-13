9e00255e8f1232731d8c0fbbe0291aaacef48cbe
kektola70-sketch <kektola70@gmail.com>
2026-02-13 07:58:04 +0300

    update src/main/java/com/gamersencqtolik/blocks/ModItems.java

--- src/main/java/com/gamersencqtolik/blocks/ModItems.java
+++ src/main/java/com/gamersencqtolik/blocks/ModItems.java
@@ -9,14 +9,14 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "simmod");
 
     public static final RegistryObject<Item> SIM_GLASS = ITEMS.register("sim_glass",
-        () -> new BlockItem(ModBlocks.SIM_GLASS.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
+        () -> new BlockItem(ModBlocks.SIM_GLASS.get(), new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
 
     public static final RegistryObject<Item> SIM_WIRE = ITEMS.register("sim_wire",
-        () -> new BlockItem(ModBlocks.SIM_WIRE.get(), new Item.Properties().group(ItemGroup.REDSTONE)));
+        () -> new BlockItem(ModBlocks.SIM_WIRE.get(), new Item.Properties().tab(ItemGroup.TAB_REDSTONE)));
 
     public static final RegistryObject<Item> CODE_MACHINE = ITEMS.register("code_machine",
-        () -> new BlockItem(ModBlocks.CODE_MACHINE.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
+        () -> new BlockItem(ModBlocks.CODE_MACHINE.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
 
     public static final RegistryObject<Item> SIM_PROJECTOR = ITEMS.register("sim_projector",
-        () -> new BlockItem(ModBlocks.SIM_PROJECTOR.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
+        () -> new BlockItem(ModBlocks.SIM_PROJECTOR.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
 }
